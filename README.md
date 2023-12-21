f9a94cfaf3568375924535bf4f019f747dbb4caf
Amit <3540115+amitdeshmukh@users.noreply.github.com>
2023-12-21 10:26:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# GCPCrypto Module
+
+The GCPCrypto module provides a set of methods to interact with Google Cloud's Key Management Service (KMS) and Secret Manager. It is designed to create, encrypt, store, and decrypt cryptographic keys using Google Cloud's services.
+
+## How it Works
+The GCPCrypto class is instantiated with a Google Cloud project ID, location ID, and a key ring. It uses these to interact with Google Cloud's KMS and Secret Manager.
+
+The class provides the following methods:
+
+- `createCryptoKey(keyId, protectionLevel)`: This method creates a new CryptoKey within the specified KeyRing in Google Cloud KMS. The key ID and protection level (e.g., SOFTWARE or HSM) are required parameters.
+
+- `encryptAndStoreSecretKey(keyId, aesKey, overwrite)`: This method encrypts the supplied AES-256 symmetric key using Cloud KMS, and stores the encrypted key in Google Secret Manager. The `keyId` and `aesKey` are required parameters. The overwrite parameter is optional and defaults to `false`. If overwrite is `true` and the secret already exists, it will be deleted before the new secret is stored.
+
+- `decryptSecretKey(keyId)`: This method retrieves the encrypted key from Google Secret Manager and decrypts it using Cloud KMS. The `keyId` is a required parameter.
+
+## Authentication
+
+The module uses Application Default Credentials (ADC) to authenticate with Google Cloud services. ADC is a strategy that allows the module to find and use appropriate credentials based on its environment. This could be credentials set in an environment variable, credentials provided by the Google Cloud SDK, credentials provided by the Google Cloud Metadata server, etc.
+
+Please ensure that the environment where this module is used is configured with appropriate credentials that have necessary permissions to interact with Google Cloud KMS and Secret Manager. More information on ADC can be found [here](https://cloud.google.com/docs/authentication/provide-credentials-adc).
+
+
+## Installation
+
+```bash
+npm install @amitdeshmukh/gcp-crypto
+# or
+yarn add @amitdeshmukh/gcp-crypto
+```
+
+## Usage
+Once installed, you can import and use the module like this:
+
+```js
+import GCPCrypto from 'gcp-crypto';
+
+// Initialize the module with your project ID, GCP keyring location ID, and keyring name
+const gcpCrypto = new GCPCrypto('your-project-id', 'your-location-id', 'your-key-ring');
+
+
+// Create a CryptoKey in your KeyRing
+await gcpCrypto.createCryptoKey(keyId, protectionLevel);
+
+// Generate a random AES-256-GCM symmetric encryption/decryption key
+import Cryptr from 'cryptr';
+const cryptr = new Cryptr(process.env.SECRET);
+const aesKey = cryptr.encrypt(Math.random().toString(36).substring(2, 15));
+
+// Encrypt the AES key with GCP KMS and store it in Secret Manager
+const encryptedKey = await gcpCrypto.encryptAndStoreSecretKey(keyId, aesKey, true);
+console.log('Encrypted key:', encryptedKey.toString('base64'));
+
+// Retrieve the key from Secret Manager and decrypt it using GCP KMS
+const decryptedKey = await gcpCrypto.decryptSecretKey(keyId);
+console.log('Decrypted key:', decryptedKey);
+
+```
---
+++ src/index.js
@@ -0,0 +1,164 @@
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import { KeyManagementServiceClient } from '@google-cloud/kms';
+
+// Instantiates a Secret Manager client
+const secretmanagerClient = new SecretManagerServiceClient();
+
+// Instantiates a KMS client
+const kmsClient = new KeyManagementServiceClient();
+
+class GCPCrypto {
+  constructor(projectId, locationId, keyRing) {
+    this.projectId = projectId;
+    this.locationId = locationId;
+    this.keyRing = keyRing;
+    this.secretmanagerClient = new SecretManagerServiceClient();
+    this.kmsClient = new KeyManagementServiceClient();
+  }
+
+  /**
+   * Creates a new CryptoKey within the specified KeyRing.
+   *
+   * @param {string} keyId - The key ID to store the key under.
+   * @param {string} protectionLevel - The protection level for the CryptoKey eg, SOFTWARE or HSM.
+   * @throws {Error} Will throw an error if the CryptoKey creation fails.
+   * @returns {Promise<Object>} A promise that resolves to the newly created CryptoKey.
+   */
+  async createCryptoKey(keyId, protectionLevel) {
+    try {
+      const [cryptoKey] = await kmsClient.createCryptoKey({
+        parent: kmsClient.keyRingPath(projectId, locationId, keyRing),
+        cryptoKeyId: keyId,
+        cryptoKey: {
+          purpose: 'ENCRYPT_DECRYPT',
+          versionTemplate: {
+            algorithm: 'GOOGLE_SYMMETRIC_ENCRYPTION',
+            protectionLevel: protectionLevel,
+          },
+        },
+      });
+
+      return cryptoKey;
+    } catch (error) {
+      if (error.code === 6) {
+        throw new Error(`KMS CryptoKey ${keyId} already exists in keyring ${keyRing}`);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  /**
+   * Create a random AES-256 encryption key
+   * Encrypt the key using Cloud KMS
+   * Store the encrypted key in Google Secret Manager
+   * @param {string} keyId The key ID to be used to store the key
+   * @param {string} aesKey The AES key
+   * @param {boolean} overwrite Overwrite the secret in Google Secret Manager if it already exists
+   * @throws {Error} If the encrypt operation fails
+   * @returns {Promise<string>} The encrypted key
+   * */
+  async encryptAndStoreSecretKey(keyId, aesKey, overwrite = false) {
+    // Build the key name
+    const keyName = kmsClient.cryptoKeyPath(
+      projectId,
+      locationId,
+      keyRing,
+      keyId
+    );
+
+    // Check if the secret already exists
+    const secretId = `${keyId}-aes-key`;
+    const [secrets] = await secretmanagerClient.listSecrets({
+      parent: `projects/${projectId}`,
+    });
+    const secretExists = secrets.some(secret => secret.name.includes(secretId));
+
+    // If the secret exists and overwrite flag is set, delete the secret
+    if (secretExists && overwrite) {
+      await secretmanagerClient.deleteSecret({
+        name: `projects/${projectId}/secrets/${secretId}`,
+      });
+    }  
+
+    // Encrypt the key using Cloud KMS
+    const [encryptResponse] = await kmsClient.encrypt({
+      name: keyName,
+      plaintext: Buffer.from(aesKey).toString('base64'),
+    });
+    const encryptedKey = encryptResponse.ciphertext;
+
+    // Store the encrypted key in Google Secret Manager
+    try {
+      const [secret] = await secretmanagerClient.createSecret({
+        parent: `projects/${projectId}`,
+        secretId: `${keyId}-aes-key`,
+        secret: {
+          replication: {
+            automatic: {},
+          },
+          labels: {
+            keyid: keyId,
+          },
+        },
+      });
+
+      const [version] = await secretmanagerClient.addSecretVersion({
+        parent: secret.name,
+        payload: {
+          data: Buffer.from(encryptedKey, 'base64'),
+        },
+      });
+      console.log('Added secret version:', version.name);
+    } catch (error) {
+      if (error.code === 6) {
+        throw new Error('Secret already exists');
+      } else {
+        throw error;
+      }
+    }
+
+    return encryptedKey;
+  }
+
+  /**
+   * Retrieve the encrypted key from Google Secret Manager
+   * Decrypt the key using Cloud KMS
+   * @param {string} keyId The key ID to retrieve the key from
+   * @throws {Error} If the decrypt operation fails
+   * @returns {Promise<string>} The decrypted key
+   * */
+  async decryptSecretKey(keyId) {
+    try {
+      // Build the key name
+      const keyName = kmsClient.cryptoKeyPath(
+        projectId,
+        locationId,
+        keyRing,
+        keyId
+      );
+
+      // Build the secret name
+      const secretId = `${keyId}-aes-key`;
+      const secretName = `projects/${projectId}/secrets/${secretId}/versions/latest`;
+
+      // Retrieve the encrypted key from Google Secret Manager
+      const [version] = await secretmanagerClient.accessSecretVersion({
+        name: secretName,
+      });
+      const encryptedKey = version.payload.data;
+
+      // Decrypt the key using Cloud KMS
+      const [decryptResponse] = await kmsClient.decrypt({
+        name: keyName,
+        ciphertext: encryptedKey,
+      });
+
+      return decryptResponse.plaintext.toString('utf8');    
+    } catch (error) {
+      throw new Error('Failed to obtain the key:', error);
+    }
+  }
+}
+
+export default GCPCrypto;
